51958819ad81c3ceb6ec691d8beaf9ceb74c423d
Shahbaz Ali <shahbazdeveloper824@gmail.com>
2021-12-23 04:33:26 +0500

    Angular Data Binding for Register Form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  public user = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+
+  };
+
+
   ngOnInit(): void {
+  }
+
+  formSubmit() {
+    console.log(this.user);
+    if(this.user.username=='' || this.user.username==null){
+    alert('user is required')
+    }
   }
 
 }
